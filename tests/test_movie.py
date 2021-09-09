3a41e92e3f29b45f5a89ac303d9d9bc7f03d502d
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-09-09 12:02:23 +0300

    deployment to heroku

---
+++ tests/test_movie.py
@@ -0,0 +1,20 @@
+import unittest
+from app.models import Movie
+
+class MovieTest(unittest.TestCase):
+    '''
+    Test Class to test the behavior of the Movie class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_movie,Movie))
+
+
+# if __name__ == '__main__':
+#     unittest.main()
